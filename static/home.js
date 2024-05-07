e817f8ec0731697ca2714e94c2bd81cd5ddf826a
nmro184 <nimrod.balaban@gmail.com>
2024-05-07 23:39:27 +0300

    ...

--- static/home.js
+++ static/home.js
@@ -2,11 +2,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 
 function Header(){
+    const createRecipe = () => {
+        window.location.href = '/'
+    };
     return (
         <header>
             <h2 className="welcome">
                 Welcome {username}
                 <button> my recipes </button>
+                <button className = "add-recipe-button" onClick = {createRecipe}>+</button>
             </h2>
           
           <div className="search">
@@ -16,10 +20,32 @@ function Header(){
         </header>
       );
 }
+function Main(){
+    return(
+        <RecipeCard/>
+    )
+}
+function RecipeCard(){
+    return(
+        <div className = 'recipe-card'>
+            <image></image>
+            <h2>title</h2>
+            <div className = "descriptors">
+                <h4>time</h4>
+                <h4>diffculty</h4>
+                <h4> kosher </h4>
+                <h4> special descriptor </h4>
+            </div>
 
+        </div>
+    )
+}
 function App(){
     return(
-        <Header/>
+        <>
+            <Header/>
+            <Main/>
+        </>
     )
 }
 
