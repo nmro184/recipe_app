5ccb0e91aadb51edf310df8604979e9e7434a5e2
nmro184 <nimrod.balaban@gmail.com>
2024-05-14 22:33:57 +0300

    started testing

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 import requests
 import json
 base_url = "http://127.0.0.1:5000"
-
+from db import delete_recipe
 
 username = "test"
 def test_sign_up():
@@ -18,7 +18,7 @@ def test_sign_up():
 
 
 def test_create():
-    recipe = {
+    test_recipe = {
         'image': 101010,
         'title': 'test',
         'description': 'test',
@@ -29,9 +29,34 @@ def test_create():
         'author':'test'
     }
     headers = {'Content-Type': 'application/json'}
-    response = requests.post(url = base_url+'/new_recipe' , data=json.dumps(recipe) , headers= headers)
+    response = requests.post(url = base_url+'/new_recipe' , data=json.dumps(test_recipe) , headers= headers)
     assert response.status_code == 200
     response = requests.get(url = base_url+'/get_all_recipes')
     recipes = response.json()['recipes']
+    
+    flag = False
     for recipe in recipes:
-        print('s')
+        if recipe['title'] == test_recipe['title']:
+            flag = True
+    assert flag == True
+    
+
+
+def test_delete():
+    response = requests.get(url = base_url+'/get_all_recipes')
+    recipes = response.json()['recipes']
+    recipe_id = 0
+    flag = False
+    for recipe in recipes:
+        if recipe['title'] == 'test':
+            flag = True
+            recipe_id = recipe['id']
+    assert flag == True
+    delete_recipe(recipe_id)
+    response = requests.get(url = base_url+'/get_all_recipes')
+    recipes = response.json()['recipes']
+    flag = False
+    for recipe in recipes:
+        if recipe['title'] == 'test':
+            flag = True
+    assert flag == False
